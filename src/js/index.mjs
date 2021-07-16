f18000beb0216736dd010d4c756131a111debd44
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-16 15:03:07 +0300

    add missing server worker in index.mjs

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -20,3 +20,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     router.useRouter()
 })
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('sw.js')
+        .then((registration) => {
+            console.log('SW Registered!')
+            console.log(registration)
+        })
+        .catch((error) => {
+            console.log('SW Registtration failed!')
+            console.log(error)
+        })
+}
